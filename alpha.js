9a67b4830433b1c0bf50199d7f2dd91b73e10ef6
Infinite <29018679+InfiniteX95@users.noreply.github.com>
2022-05-12 23:05:45 +0200

    Javascript updated

--- alpha.js
+++ alpha.js
@@ -1,28 +1,4 @@
 function alpha(item) {
-    var input = document.conversion.textarea;
-    if (document.selection) {
-        //input.focus();
-        range = document.selection.createRange();
-        range.text = item;
-        range.select();
-    }
-    else if (input.selectionStart || input.selectionStart === '0') {
-        var startPos = input.selectionStart;
-        var endPos = input.selectionEnd;
-        var cursorPos = startPos;
-        //var scrollTop = input.scrollTop;
-        var baselength = 0;
-        input.value = input.value.substring(0, startPos)
-            + item
-            + input.value.substring(endPos, input.value.length);
-        cursorPos += item.length;
-        //input.focus();
-        input.selectionStart = cursorPos;
-        input.selectionEnd = cursorPos;
-        //input.scrollTop = scrollTop;
-    }
-    else {
-        input.value += item;
-        //input.focus();
-    }
+    var input = document.getElementById("field");
+    input.value = input.value + item;
 }
